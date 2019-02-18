0cbbfa3402f9e72492b2c442708adf9f174ae0e0
vagrant <vagrant@localhost.localdomain>
2019-02-18 22:09:38 +0000

    Change post variable to instance variable to fix bug

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -17,7 +17,7 @@ class PostsController < ApplicationController
   def create
     @post = current_user.posts.build(post_params)
     if @post.save
-      redirect_to root_url, notice: "The post \"#{post.content}\" was successfully created"
+      redirect_to root_url, notice: "The post \"#{@post.content}\" was successfully created"
     else
       render 'new'
     end
